b431aec290f21a433fa017bf416c1df40917c481
nunjayi <epenyash@gmail.com>
2024-04-03 22:34:17 +0300

    editing index .js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(driverArr, name) {
+    return driverArr.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(driverArr, letters) {
+    const lowerCaseLetters = letters.toLowerCase();
+    return driverArr.filter(driver => driver.toLowerCase().startsWith(lowerCaseLetters));
+}
+
+function matchName(driverObj, string) {
+    return driverObj.filter(driver => driver.name.toLowerCase() === string.toLowerCase());
+}
